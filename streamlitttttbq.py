ab3fe60c5f4ab89312905b9f00cf25f2e470a8fc
Bartłomiej Szeliga <102485637+Szelig38@users.noreply.github.com>
2024-11-28 16:59:18 +0100

    streamlitttttbq.py

--- streamlitttttbq.py
+++ streamlitttttbq.py
@@ -16,23 +16,3 @@ credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
 
-# Utwórz klienta BigQuery
-client = bigquery.Client(credentials=credentials)
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.cache_data to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
-
-rows = run_query("SELECT * FROM `sst-sandbox-356409.logs.appengine_googleapis_com_nginx_health_check_20241128` LIMIT 10")
-
-# Print results.
-st.write("Przykładowe dane z naszego BQ")
-for row in rows:
-    st.write("✍️ " + row['*'])
-
