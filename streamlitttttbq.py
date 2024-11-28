9b2116f4f2f7f01c1c6fa9e69084973611dc7767
Bartłomiej Szeliga <102485637+Szelig38@users.noreply.github.com>
2024-11-28 16:36:23 +0100

    streamlitttttbq.py

--- streamlitttttbq.py
+++ streamlitttttbq.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/132cQPCN_WH3-zLwwRNsAYUbVdufKJdb2
 """
 
-!pip install streamlit
-
 import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
