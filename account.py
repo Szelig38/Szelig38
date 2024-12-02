eb4448ed3662b5544d8c427e25b816796334a036
Bartłomiej Szeliga <102485637+Szelig38@users.noreply.github.com>
2024-12-02 16:00:45 +0100

    account.py

--- account.py
+++ account.py
@@ -8,7 +8,7 @@ import requests
 
 
 cred = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
+    st.secrets["gcp_service_account"])
 client = bigquery.Client(credentials=credentials)
 
 def app():
