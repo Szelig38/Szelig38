1877b64bc3da84b56971c0a53c07a1cda4e03a27
Bartłomiej Szeliga <102485637+Szelig38@users.noreply.github.com>
2024-12-02 12:47:34 +0100

    Trending.py

--- Trending.py
+++ Trending.py
@@ -1,7 +1,3 @@
 import streamlit as st
-
 def app():
-    st.subheader('Trending thoughts')
-
-    st.text_area(label='#',value='Please wait this page is under construction..')
-    
+        st.write('Trending')
