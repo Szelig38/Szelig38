92918efb54aece134a424699e51a9333dcb82428
Bartłomiej Szeliga <102485637+Szelig38@users.noreply.github.com>
2024-12-02 15:41:26 +0100

    streamlitttttbq.py

--- streamlitttttbq.py
+++ streamlitttttbq.py
@@ -9,20 +9,10 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-from google.oauth2 import service_account
-from google.cloud import bigquery
 from streamlit_option_menu import option_menu
 import os
-
-
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-
-# UtwĂłrz klienta BigQuery
-client = bigquery.Client(credentials=credentials)
-
 import Home, Trending, Account, GA4, GoogleAds, Ecommerce
+
 st.set_page_config(
         page_title="JohnLeggy",
 )
