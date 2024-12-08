e7c729aa3787378f5a307b783ff5f97a6b13b115
Bartłomiej Szeliga <102485637+Szelig38@users.noreply.github.com>
2024-12-08 18:53:18 +0100

    streamlitttttbq.py

--- streamlitttttbq.py
+++ streamlitttttbq.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from streamlit_option_menu import option_menu
 
-import home, trending, account, your, about, buy_me_a_coffee
+import home,account,trending,your,about,buy_me_a_coffee
 
 import os
 from dotenv import load_dotenv
